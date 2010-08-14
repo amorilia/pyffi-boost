e7711242cf472d26e01769ae27067c9e6928689c
Amorilia <amorilia@users.sourceforge.net>
2010-08-14 15:08:03 +0100

    Moved generate tests to separate file.

--- test/pyffi/object_models/ast_generate_test.cpp
+++ test/pyffi/object_models/ast_generate_test.cpp
@@ -45,61 +45,63 @@ using boost::get;
 using namespace pyffi;
 using namespace pyffi::object_models;
 
-BOOST_AUTO_TEST_SUITE(ast_test_suite)
+BOOST_AUTO_TEST_SUITE(ast_generate_test_suite)
 
-BOOST_AUTO_TEST_CASE(ast_attr_test)
-{
-    Attr x("Int", "x");
-    BOOST_CHECK_EQUAL(x.name, "x");
-    BOOST_CHECK_EQUAL(x.class_name, "Int");
-}
-
-BOOST_AUTO_TEST_CASE(ast_class_test)
-{
-    Class int_("Int");
-    BOOST_CHECK_EQUAL(int_.name, "Int");
-    BOOST_CHECK(!int_.base_name);
-    BOOST_CHECK(!int_.scope);
-    int_.base_name = "Object";
-    int_.scope = Scope();
-    int_.scope.get().push_back(Attr("Bool", "negate"));
-    BOOST_CHECK_EQUAL(int_.base_name.get(), "Object");
-    BOOST_CHECK_EQUAL(int_.scope.get().size(), 1);
-    BOOST_CHECK_EQUAL(get<Attr>(int_.scope.get()[0]).name, "negate");
-    BOOST_CHECK_EQUAL(get<Attr>(int_.scope.get()[0]).class_name, "Bool");
-}
-
-BOOST_AUTO_TEST_CASE(ast_ifelifelse_test)
-{
-    IfElifsElse ifelifselse;
-    ifelifselse.if_.expr = false;
-    ifelifselse.if_.scope.push_back(Attr("Int", "x1"));
-    ifelifselse.elifs_.resize(2);
-    ifelifselse.elifs_[0].expr = true;
-    ifelifselse.elifs_[0].scope.push_back(Attr("Int", "x2"));
-    ifelifselse.elifs_[1].expr = true;
-    ifelifselse.elifs_[1].scope.push_back(Attr("Int", "x3"));
-    ifelifselse.else_.push_back(Attr("Int", "x4"));
-}
-
-BOOST_AUTO_TEST_CASE(ast_scope_test)
+BOOST_AUTO_TEST_CASE(ast_generate_class_test)
 {
     Scope scope;
     Class int_("Int");
     scope.push_back(int_);
-    Class intvec("IVector");
+    std::ostringstream os;
+    generate(os, scope);
+    BOOST_CHECK_EQUAL(os.str(), "class Int\n");
+}
+
+BOOST_AUTO_TEST_CASE(ast_generate_base_name_test)
+{
+    Scope scope;
+    Class int_("Int");
+    int_.base_name = "Object";
+    scope.push_back(int_);
+    std::ostringstream os;
+    generate(os, scope);
+    BOOST_CHECK_EQUAL(os.str(), "class Int(Object)\n");
+}
+
+BOOST_AUTO_TEST_CASE(ast_generate_attr_test)
+{
+    Scope scope;
     Attr x("Int", "x");
-    Attr y("Int", "y");
-    Attr z("Int", "z");
-    intvec.scope = Scope();
-    intvec.scope.get().push_back(x);
-    intvec.scope.get().push_back(y);
-    intvec.scope.get().push_back(z);
-    scope.push_back(intvec);
-    IfElifsElse ifelifselse;
-    ifelifselse.if_.expr = true;
-    ifelifselse.if_.scope.push_back(Attr("IVector", "position"));
-    scope.push_back(ifelifselse);
+    scope.push_back(x);
+    std::ostringstream os;
+    generate(os, scope);
+    BOOST_CHECK_EQUAL(os.str(), "Int x\n");
+}
+
+BOOST_AUTO_TEST_CASE(ast_generate_class_attr_test)
+{
+    Scope scope;
+    Class int_("Int");
+    Attr x("Int", "x");
+    scope.push_back(int_);
+    scope.push_back(x);
+    std::ostringstream os;
+    generate(os, scope);
+    BOOST_CHECK_EQUAL(os.str(), "class Int\nInt x\n");
+}
+
+BOOST_AUTO_TEST_CASE(ast_generate_class_scope_test)
+{
+    Class vec("Vector");
+    Attr x("Int", "x");
+    vec.scope = Scope();
+    vec.scope.get().push_back(x);
+    BOOST_CHECK_EQUAL(vec.scope.get().size(), 1);
+    Scope scope;
+    scope.push_back(vec);
+    std::ostringstream os;
+    generate(os, scope);
+    BOOST_CHECK_EQUAL(os.str(), "class Vector:\n    Int x\n");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
