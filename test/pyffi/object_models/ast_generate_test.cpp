867c86df71c9296454ece639def0daaaaccecebb
Amorilia <amorilia@users.sourceforge.net>
2010-12-21 23:21:43 +0000

    Link against boost test instead of direct include.

--- test/pyffi/object_models/ast_generate_test.cpp
+++ test/pyffi/object_models/ast_generate_test.cpp
@@ -37,7 +37,7 @@ POSSIBILITY OF SUCH DAMAGE.
 
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MAIN
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 
 #include "pyffi/object_models/ast.hpp"
 
